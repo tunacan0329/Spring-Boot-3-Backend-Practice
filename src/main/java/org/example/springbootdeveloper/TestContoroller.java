369ecd8c2a59ea3706147b3b7fac65beb77c8c4b
tunacan0329 <tunacan0329@gmail.com>
2024-03-13 14:09:25 +0900

    feat: 컨트롤러, 서비스, 레포지토리 구조 이해하기

--- src/main/java/org/example/springbootdeveloper/TestContoroller.java
+++ src/main/java/org/example/springbootdeveloper/TestContoroller.java
@@ -1,12 +1,20 @@
 package org.example.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestContoroller {
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
-    public String test(){
-        return "Hello world!";
+    public List<Member> getAllMembers(){
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
+
 }
